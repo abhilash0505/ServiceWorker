69d71e94093c554b1e945c8cd91d2c38ddcf121f
Abhilash Gopal <abhilash0505@gmail.com>
2015-12-14 12:39:09 +0530

    test

--- test/examples/1/sw.js
+++ test/examples/1/sw.js
@@ -14,64 +14,48 @@ if(typeof self.CACHE_NAME !== 'string') {
 }
 
 self.addEventListener('fetch', function(event) {
-	console.log(event.request);
-	// Clone the request for fetch and cache
-	// A request is a stream and can be consumed only once.
-	var fetchRequest = event.request.clone(),
-		cacheRequest = event.request.clone();
 
 	// Respond with content from fetch or cache
-	event.respondWith(
-  caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+              // Cache hit - return response
+              if (response) {
+                console.log("from Cache");
+                return response;
+              }
 
-	  
+              // IMPORTANT: Clone the request. A request is a stream and
+              // can only be consumed once. Since we are consuming this
+              // once by cache and once by the browser for fetch, we need
+              // to clone the response
+              var fetchRequest = event.request.clone();
 
-			// Try fetch
-			fetch(fetchRequest)
+              return fetch(fetchRequest).then(
+                  function(response) {
+                    // Check if we received a valid response
+                    if(!response || response.status !== 200 || response.type !== 'basic') {
+                      return response;
+                    }
 
-				// when fetch is successful, we update the cache
-				.then(function(response) {
+                    // IMPORTANT: Clone the response. A response is a stream
+                    // and because we want the browser to consume the response
+                    // as well as the cache consuming the response, we need
+                    // to clone it so we have 2 stream.
+                    var responseToCache = response.clone();
 
-					// A response is a stream and can be consumed only once.
-					// Because we want the browser to consume the response,
-					// as well as cache to consume the response, we need to
-					// clone it so we have 2 streams
-					var responseToCache = response.clone();
-
-					// and update the cache
-					caches
-						.open(self.CACHE_NAME)
-						.then(function(cache) {
-
-							// Clone the request again to use it
-							// as the key for our cache
-							var cacheSaveRequest = event.request.clone();
-							cache.put(cacheSaveRequest, responseToCache);
-
-						});
-
-					// Return the response stream to be consumed by browser
-					return response;
-
-				})
-
-				// when fetch times out or fails
-				.catch(function(err) {
-
-					// Return the promise which
-					// resolves on a match in cache for the current request
-					// ot rejects if no matches are found
-					return caches.match(cacheRequest);
-
-				})
-			
-			});
-	);
+                    caches.open(CACHE_NAME)
+                        .then(function(cache) {
+                          cache.put(event.request, responseToCache);
+                        });
+                    console.log("Not from Cache");
+                    return response;
+                  }
+              );
+            })
+    );
+  });
 });
 
 // Now we need to clean up resources in the previous versions
