c1ddc4965cbd6179630037deecad8ba192e822f5
Abhilash Gopal <abhilash0505@gmail.com>
2015-12-14 12:45:27 +0530

    test

--- test/examples/1/main.js
+++ test/examples/1/main.js
@@ -1,18 +1,13 @@
-$(function() {
-	$(function(){
-		var url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Pleiades_large.jpg/1024px-Pleiades_large.jpg';
-		for(var i=0; i < 10; i++){
-				$('body').append('<img src="'+url+'?rand='+i+'"/>');
-		}
-	});
-});
-
 if(navigator.serviceWorker) {
 	navigator
 		.serviceWorker
 		.register('./sw.js')
 		.then(function(r) {
 			console.log('Pics are now available offline');
+			var url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Pleiades_large.jpg/1024px-Pleiades_large.jpg';
+			for(var i=0; i < 10; i++){
+				$('body').append('<img src="'+url+'?rand='+i+'"/>');
+			}
 		})
 		.catch(function(e) {
 			console.log('Pics are NOT available offline');
